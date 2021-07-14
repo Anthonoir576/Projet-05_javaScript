cb618ecd6e27b196d21fa26061fa5a923bb0888c
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-07-14 14:57:31 +0200

    End

--- source/js/index.js
+++ source/js/index.js
@@ -1,37 +1,57 @@
-// Variable et constante
-let startMinutes = 2;
-let time = startMinutes * 60;
-
-const countDownElement = document.getElementById('countdown');
-
-
-
 // Fonction
-function updateCountDown() {
+function getInputValue() {
+    // on recupere le choix utilisateur
+    let choixUtilisateur = document.getElementById('choice').value;
 
-    let minutes = Math.floor(time / 60);
-    let seconds = time % 60;
+    if (isNaN(choixUtilisateur)) {
 
-    // condition en ternaire, pour rajouté un zero, au chiffre inférieur a dix
-    seconds = seconds < 10 ? '0' + seconds : seconds;
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-
-    // recupere la variable minutes
-    countDownElement.innerHTML = `${minutes} : ${seconds}`;
-
-    // afin de pas ce retrouver avec des moins 10 ond donne une condition, si le chiffre et superieur a 0 alors on enleve 1, sinon cest que le compte a rebour et fini, et on l'annonce
-    if(time > 0) {
-
-        // Enleve des seconds, relié a l'interval, ou chaque second cette fonction se lance, et en enleve une
-        time--;
+        alert('Ceci n\'est pas un nombre !')
 
     } else {
 
-        countDownElement.innerHTML = ` C'EST FINI !`;
+        let startMinutes = choixUtilisateur;
+        this.choice.value = ('');
+
+        // Variable et constante
+        let time = startMinutes * 60;
+
+        const countDownElement = document.getElementById('countdown');
+
+        // Interval
+        setInterval(updateCountDown, 1000);
+
+        function updateCountDown() {
+
+            let minutes = Math.floor(time / 60);
+            let seconds = time % 60;
+        
+            // condition en ternaire, pour rajouté un zero, au chiffre inférieur a dix
+            seconds = seconds < 10 ? '0' + seconds : seconds;
+            minutes = minutes < 10 ? '0' + minutes : minutes;
+        
+            // recupere la variable minutes
+            countDownElement.innerHTML = `${minutes} : ${seconds}`;
+        
+            // afin de pas ce retrouver avec des moins 10 ond donne une condition, si le chiffre et superieur a 0 alors on enleve 1, sinon cest que le compte a rebour et fini, et on l'annonce
+            if(time > 0) {
+        
+                // Enleve des seconds, relié a l'interval, ou chaque second cette fonction se lance, et en enleve une
+                time--;
+        
+            } else {
+        
+                countDownElement.innerHTML = ` C'EST FINI !`;
+        
+            };
+        
+        };
 
     };
 
 };
 
-// Interval
-setInterval(updateCountDown, 1000);
+
+
+
+
+
